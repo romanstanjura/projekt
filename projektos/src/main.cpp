cfdf45cc812a4f6242870c6e206fb8652f4a14af
rstanjura <55387906+romanstanjura@users.noreply.github.com>
2020-01-13 13:54:00 +0100

    Část změny kódu na pole (matice)

--- projektos/src/main.cpp
+++ projektos/src/main.cpp
@@ -4,56 +4,61 @@
 #define S1 3
 #define S2 4
 #define S3 5
-#define sensorOut 6
-Servo topServo;
-Servo bottomServo;
-int color=0;
-int frequency = 0;
+#define senzor 6
+// #define POCET_BAREV 6
+Servo prvniServo;
+Servo druheServo;
+int barva=0;
+//int barva;
+int frekvenceBarvy = 0;
+// int rozsahBarev[POCET_BAREV][6]={{32,45,55,65,-1,1}, {-1,1,43,55,35,47}, {40,53,40,53,-1,1}, {24,38,30,44,-1,1}, {46,56,55,65,-1,1}, {-1,1,45,58,26,40}}
+//int i;
+//int R,G,B;
 
-int readColor() {
+int cteniBarvy() {
   digitalWrite(S2, LOW);
   digitalWrite(S3, LOW);
-  frequency = pulseIn(sensorOut, LOW);
-  int R = frequency;
+  frekvenceBarvy = pulseIn(senzor, LOW);
+  int R = frekvenceBarvy;
   Serial.print("R= ");
-  Serial.print(frequency);
+  Serial.print(frekvenceBarvy);
   Serial.print("  ");
   delay(50);
   digitalWrite(S2, HIGH);
   digitalWrite(S3, HIGH);
-  frequency = pulseIn(sensorOut, LOW);
-  int G = frequency;
+  frekvenceBarvy = pulseIn(senzor, LOW);
+  int G = frekvenceBarvy;
   Serial.print("G= ");
-  Serial.print(frequency);
+  Serial.print(frekvenceBarvy);
   Serial.print("  ");
   delay(50);
   digitalWrite(S2, LOW);
   digitalWrite(S3, HIGH);
-  frequency = pulseIn(sensorOut, LOW);
-  int B = frequency;
+  frekvenceBarvy = pulseIn(senzor, LOW);
+  int B = frekvenceBarvy;
   Serial.print("B= ");
-  Serial.print(frequency);
+  Serial.print(frekvenceBarvy);
   Serial.println("  ");
   delay(50);
   if((R<45 && R>32) && (G<65 && G>55)){
-    color = 1; // červená
+    barva = 1; // červená
   }
   if((G<55 && G>43) && (B<47 && B>35)){
-    color = 2; // oranžová
+    barva = 2; // oranžová
   }
   if((R<53 && R>40) && (G<53 && G>40)){
-    color = 3; // zelená
+    barva = 3; // zelená
   }
   if((R<38 && R>24) && (G<44 && G>30)){
-    color = 4; // žlutá
+    barva = 4; // žlutá
   }
   if((R<56 && R>46) && (G<65 && G>55)){
-    color = 5; // hnědá
+    barva = 5; // hnědá
   }
   if ((G<58 && G>45) && (B<40 && B > 26)){
-    color = 6; // modrá
+    barva = 6; // modrá
   }
-  return color;  
+  return barva;  
 }
 
 void setup() {
@@ -61,59 +66,59 @@ void setup() {
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
-  pinMode(sensorOut, INPUT);
+  pinMode(senzor, INPUT);
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
-  topServo.attach(7);
-  bottomServo.attach(8);
+  prvniServo.attach(7);
+  druheervo.attach(8);
   Serial.begin(9600);
 }
 void loop() {
-  topServo.write(115);
+  prvniServo.write(115);
   delay(500);
   
   for(int i = 115; i > 65; i--) {
-    topServo.write(i);
+    prvniServo.write(i);
     delay(2);
   }
   delay(500);
   
-  color = readColor();
+  barva = cteniBarvy();
   delay(10);  
-  switch (color) {
+  switch (barva) {
     case 1:
-    bottomServo.write(50);
-    break;
+      druheServo.write(50);
+      break;
     case 2:
-    bottomServo.write(75);
-    break;
+      druheServo.write(75);
+      break;
     case 3:
-    bottomServo.write(100);
-    break;
+      druheServo.write(100);
+      break;
     case 4:
-    bottomServo.write(125);
-    break;
+      druheServo.write(125);
+      break;
     case 5:
-    bottomServo.write(150);
-    break;
+      druheServo.write(150);
+      break;
     case 6:
-    bottomServo.write(175);
-    break;
+      druheServo.write(175);
+      break;
     
     case 0:
-    break;
+      break;
   }
   delay(300);
   
   for(int i = 65; i > 29; i--) {
-    topServo.write(i);
+    prvniServo.write(i);
     delay(2);
   } 
   delay(200);
   
   for(int i = 29; i < 115; i++) {
-    topServo.write(i);
+    prvniServo.write(i);
     delay(2);
   }
-  color=0;
+  barva=0;
 }
