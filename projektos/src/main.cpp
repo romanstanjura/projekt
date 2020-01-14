6ff7c2946b1aad1de30b240a995b99d4d6b39b93
rstanjura <55387906+romanstanjura@users.noreply.github.com>
2020-01-14 18:27:37 +0100

    Add files via upload

--- projektos/src/main.cpp
+++ projektos/src/main.cpp
@@ -1,3 +1,4 @@
+//deklarace potrebnych knihoven a pinu pro modul TCS230
 #include <Servo.h>
 #include <Arduino.h>
 #define S0 2
@@ -5,75 +6,84 @@
 #define S2 4
 #define S3 5
 #define senzor 6
-// #define POCET_BAREV 6
+
 Servo prvniServo;
 Servo druheServo;
 int barva=0;
-//int barva;
 int frekvenceBarvy = 0;
-// int rozsahBarev[POCET_BAREV][6]={{32,45,55,65,-1,1}, {-1,1,43,55,35,47}, {40,53,40,53,-1,1}, {24,38,30,44,-1,1}, {46,56,55,65,-1,1}, {-1,1,45,58,26,40}}
-//int i;
-//int R,G,B;
 
 int cteniBarvy() {
+  //aktivace cervenych fotodiod modulu TCS230, aby mohli "cist" barvu
   digitalWrite(S2, LOW);
   digitalWrite(S3, LOW);
+  // "precteni" frekvence barvy
   frekvenceBarvy = pulseIn(senzor, LOW);
   int R = frekvenceBarvy;
   Serial.print("R= ");
   Serial.print(frekvenceBarvy);
   Serial.print("  ");
   delay(50);
+  
+  //aktivace zelenych fotodiod modulu TCS230, aby mohli "cist" barvu
   digitalWrite(S2, HIGH);
   digitalWrite(S3, HIGH);
+  // "precteni" frekvence barvy
   frekvenceBarvy = pulseIn(senzor, LOW);
   int G = frekvenceBarvy;
   Serial.print("G= ");
   Serial.print(frekvenceBarvy);
   Serial.print("  ");
   delay(50);
+  
+  //aktivace modrych fotodiod modulu TCS230, aby mohli "cist" barvu
   digitalWrite(S2, LOW);
   digitalWrite(S3, HIGH);
+  // "precteni" frekvence barvy
   frekvenceBarvy = pulseIn(senzor, LOW);
   int B = frekvenceBarvy;
   Serial.print("B= ");
   Serial.print(frekvenceBarvy);
   Serial.println("  ");
   delay(50);
+  
+  // porovnani vsech prectenych hodnot z fotodiod a nasledne vyhodnoceni barvy objektu
   if((R<45 && R>32) && (G<65 && G>55)){
-    barva = 1; // červená
+    barva = 1; // vyhodnoceni barvy jako cervena
   }
   if((G<55 && G>43) && (B<47 && B>35)){
-    barva = 2; // oranžová
+    barva = 2; // vyhodnoceni barvy jako oranzova
   }
   if((R<53 && R>40) && (G<53 && G>40)){
-    barva = 3; // zelená
+    barva = 3; // vyhodnoceni barvy jako zelena
   }
   if((R<38 && R>24) && (G<44 && G>30)){
-    barva = 4; // žlutá
+    barva = 4; // vyhodnoceni barvy jako zluta
   }
   if((R<56 && R>46) && (G<65 && G>55)){
-    barva = 5; // hnědá
+    barva = 5; // vyhodnoceni barvy jako hneda
   }
   if ((G<58 && G>45) && (B<40 && B > 26)){
-    barva = 6; // modrá
+    barva = 6; // vyhodnoceni barvy jako modra
   }
   return barva;  
 }
 
-void setup() {
+void setup() {	
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(senzor, INPUT);
+  // Nastaveni frekvenci skalovani barev na 20%
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
+  
   prvniServo.attach(7);
   druheervo.attach(8);
   Serial.begin(9600);
 }
 void loop() {
+   //servo motor se otoci do polohy tak, aby objekt byl pod modulem TCS230 pro nasledne cteni barev
   prvniServo.write(115);
   delay(500);
   
@@ -83,8 +93,11 @@ void loop() {
   }
   delay(500);
   
+  // cten� hodnoty barev vol�n�m funkce a ulozeni hodnot do promenne
   barva = cteniBarvy();
   delay(10);  
+  
+  // podle detekovane barvy se motor servo otaci do spravne polohy, aby objekt mohl po papirove "skluzavce" sjet do spravne nadoby/krabicky
   switch (barva) {
     case 1:
       druheServo.write(50);
@@ -110,6 +123,7 @@ void loop() {
   }
   delay(300);
   
+  //servo motor se vraci do vychozi polohy, aby mohl modul vyhodnotit dalsi objekt
   for(int i = 65; i > 29; i--) {
     prvniServo.write(i);
     delay(2);
