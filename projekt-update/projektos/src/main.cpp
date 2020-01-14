414c222dc5d12d847fc6cf26d4d7fbb32e7ba194
rstanjura <55387906+romanstanjura@users.noreply.github.com>
2020-01-14 18:51:02 +0100

    Add files via upload

--- projekt-update/projektos/src/main.cpp
+++ projekt-update/projektos/src/main.cpp
@@ -1,119 +1,145 @@
+//deklarace potrebnych knihoven a pinu pro modul TCS230
 #include <Servo.h>
 #include <Arduino.h>
 #define S0 2
 #define S1 3
 #define S2 4
 #define S3 5
-#define sensorOut 6
-Servo topServo;
-Servo bottomServo;
-int color=0;
-int frequency = 0;
+#define senzor 6
 
-int readColor() {
+Servo prvniServo;
+Servo druheServo;
+int barva;
+int frekvenceBarvy = 0;
+int i;
+int red;
+int green,
+int blue;
+int rozsahBarev[POCET_BAREV][6]={{32,45,55,65,-1,1}, {-1,1,43,55,35,47}, {40,53,40,53,-1,1}, {24,38,30,44,-1,1}, {46,56,55,65,-1,1}, {-1,1,45,58,26,40}}
+
+int cteniBarvy() {
+  //aktivace cervenych fotodiod modulu TCS230, aby mohli "cist" barvu
   digitalWrite(S2, LOW);
   digitalWrite(S3, LOW);
-  frequency = pulseIn(sensorOut, LOW);
-  int R = frequency;
+  // "precteni" frekvence barvy
+  frekvenceBarvy = pulseIn(senzor, LOW);
+  int R = frekvenceBarvy;
   Serial.print("R= ");
-  Serial.print(frequency);
+  Serial.print(frekvenceBarvy);
   Serial.print("  ");
   delay(50);
+  
+  //aktivace zelenych fotodiod modulu TCS230, aby mohli "cist" barvu
   digitalWrite(S2, HIGH);
   digitalWrite(S3, HIGH);
-  frequency = pulseIn(sensorOut, LOW);
-  int G = frequency;
+  // "precteni" frekvence barvy
+  frekvenceBarvy = pulseIn(senzor, LOW);
+  int G = frekvenceBarvy;
   Serial.print("G= ");
-  Serial.print(frequency);
+  Serial.print(frekvenceBarvy);
   Serial.print("  ");
   delay(50);
+  
+  //aktivace modrych fotodiod modulu TCS230, aby mohli "cist" barvu
   digitalWrite(S2, LOW);
   digitalWrite(S3, HIGH);
-  frequency = pulseIn(sensorOut, LOW);
-  int B = frequency;
+  // "precteni" frekvence barvy
+  frekvenceBarvy = pulseIn(senzor, LOW);
+  int B = frekvenceBarvy;
   Serial.print("B= ");
-  Serial.print(frequency);
+  Serial.print(frekvenceBarvy);
   Serial.println("  ");
   delay(50);
-  if(R<45 & R>32 & G<65 & G>55){
-    color = 1; // červená
+  
+  //zatim nejsou upraveny rozsahyBarev ([i][dane cislo])
+  // porovnani vsech prectenych hodnot z fotodiod a nasledne vyhodnoceni barvy objektu
+  for(i = 0; i < POCET_BAREV;i++){
+	  if((red > rozsahyBarev[i][0]&& red < rozsahyBarev[i][1]) && (green > rozsahyBarev[i][2] && green < rozsahyBarev[i][3]) && (blue > rozsahyBarev[i][4] && blue < rozsahyBarev[i][5])){
+	  	barva = i+1; // cervena
+	  }
+	  if((red > rozsahyBarev[i][0]&& red < rozsahyBarev[i][1]) && (green > rozsahyBarev[i][2] && green < rozsahyBarev[i][3]) && (blue > rozsahyBarev[i][4] && blue < rozsahyBarev[i][5])){
+	  	barva = i+2; // oranzova
+	  }
+	  if((red > rozsahyBarev[i][0]&& red < rozsahyBarev[i][1]) && (green > rozsahyBarev[i][2] && green < rozsahyBarev[i][3]) && (blue > rozsahyBarev[i][4] && blue < rozsahyBarev[i][5])){
+	  	barva = i+3; // zelena
+	  }
+	  if((red > rozsahyBarev[i][0]&& red < rozsahyBarev[i][1]) && (green > rozsahyBarev[i][2] && green < rozsahyBarev[i][3]) && (blue > rozsahyBarev[i][4] && blue < rozsahyBarev[i][5])){
+	  	barva = i+4; // zluta
+	  }
+	  if((red > rozsahyBarev[i][0]&& red < rozsahyBarev[i][1]) && (green > rozsahyBarev[i][2] && green < rozsahyBarev[i][3]) && (blue > rozsahyBarev[i][4] && blue < rozsahyBarev[i][5])){
+	  	barva = i+5; // hneda
+	  }
+	  if((red > rozsahyBarev[i][0]&& red < rozsahyBarev[i][1]) && (green > rozsahyBarev[i][2] && green < rozsahyBarev[i][3]) && (blue > rozsahyBarev[i][4] && blue < rozsahyBarev[i][5])){
+	  	barva = i+6; // modra
+	  }
   }
-  if(G<55 & G>43 & B<47 &B>35){
-    color = 2; // oranžová
-  }
-  if(R<53 & R>40 & G<53 & G>40){
-    color = 3; // zelená
-  }
-  if(R<38 & R>24 & G<44 & G>30){
-    color = 4; // žlutá
-  }
-  if(R<56 & R>46 & G<65 & G>55){
-    color = 5; // hnědá
-  }
-  if (G<58 & G>45 & B<40 & B > 26){
-    color = 6; // modrá
-  }
-  return color;  
 }
 
-void setup() {
+void setup() {	
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
-  pinMode(sensorOut, INPUT);
+  pinMode(senzor, INPUT);
+  // Nastaveni frekvenci skalovani barev na 20%
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
-  topServo.attach(7);
-  bottomServo.attach(8);
+  
+  prvniServo.attach(7);
+  druheervo.attach(8);
   Serial.begin(9600);
 }
 void loop() {
-  topServo.write(115);
+   //servo motor se otoci do polohy tak, aby objekt byl pod modulem TCS230 pro nasledne cteni barev
+  prvniServo.write(115);
   delay(500);
   
   for(int i = 115; i > 65; i--) {
-    topServo.write(i);
+    prvniServo.write(i);
     delay(2);
   }
   delay(500);
   
-  color = readColor();
+  // cten� hodnoty barev vol�n�m funkce a ulozeni hodnot do promenne
+  barva = cteniBarvy();
   delay(10);  
-  switch (color) {
+  
+  // podle detekovane barvy se motor servo otaci do spravne polohy, aby objekt mohl po papirove "skluzavce" sjet do spravne nadoby/krabicky
+  switch (barva) {
     case 1:
-    bottomServo.write(50);
-    break;
+      druheServo.write(50);
+      break;
     case 2:
-    bottomServo.write(75);
-    break;
+      druheServo.write(75);
+      break;
     case 3:
-    bottomServo.write(100);
-    break;
+      druheServo.write(100);
+      break;
     case 4:
-    bottomServo.write(125);
-    break;
+      druheServo.write(125);
+      break;
     case 5:
-    bottomServo.write(150);
-    break;
+      druheServo.write(150);
+      break;
     case 6:
-    bottomServo.write(175);
-    break;
+      druheServo.write(175);
+      break;
     
     case 0:
-    break;
+      break;
   }
   delay(300);
   
+  //servo motor se vraci do vychozi polohy, aby mohl modul vyhodnotit dalsi objekt
   for(int i = 65; i > 29; i--) {
-    topServo.write(i);
+    prvniServo.write(i);
     delay(2);
   } 
   delay(200);
   
   for(int i = 29; i < 115; i++) {
-    topServo.write(i);
+    prvniServo.write(i);
     delay(2);
   }
-  color=0;
+  barva=0;
 }
